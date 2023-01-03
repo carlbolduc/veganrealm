dbee9e65b111dd69b8a3830d6e4f91b8a9a0154f
Carl Bolduc <carl@codebards.io>
2023-01-02 21:55:40 -0500

    Adjust search setup

--- src/queries.sql
+++ src/queries.sql
@@ -1,3 +1,3 @@
+DROP TABLE search_recipes;
 CREATE VIRTUAL TABLE search_recipes USING fts5(recipe_id, author, url, name, photo, ingredients, tokenize = 'porter ascii');
-
 INSERT INTO search_recipes SELECT recipe_id, author, url, name, photo, ingredients FROM recipe;
